8034400f73a7ced6da27bfbe3adde8a99120a69b
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-12-15 17:14:48 +0100

    cleanup 2

--- src/main/java/my_project/model/menu/StartMenu.java
+++ src/main/java/my_project/model/menu/StartMenu.java
@@ -8,22 +8,24 @@ import java.awt.*;
 public class StartMenu extends MenuUnderPoint {
 
     public StartMenu(double y){
-        this.y=y;
-        sY=y;
+        this.y = y;
+        sY = y;
         //Todo fix sY
     }
 
     public void draw(DrawTool drawTool){
         drawTool.setCurrentColor(Color.GREEN);
-        drawTool.drawFilledRectangle(((Config.WINDOW_WIDTH-130)/2+130)-110,y+30,220,50);
-        drawTool.setCurrentColor(Color.black);
-        drawTool.drawRectangle(((Config.WINDOW_WIDTH-130)/2+130)-110,y+30,220,50);
-        drawTool.formatText(" ",1,20);
-        drawTool.drawText((double)(((Config.WINDOW_WIDTH-130)/2+130)-100),y+65,"neues Spiel beginnen");
+        drawTool.drawFilledRectangle(((Config.WINDOW_WIDTH - 130)/2f + 130) - 110, y + 30, 220, 50);
+
+        drawTool.setCurrentColor(Color.BLACK);
+        drawTool.drawRectangle(((Config.WINDOW_WIDTH - 130)/2f + 130) - 110, y + 30, 220, 50);
+
+        drawTool.formatText("", 1, 20);
+        drawTool.drawText(((Config.WINDOW_WIDTH - 130)/2f + 130) - 100, y + 65, "start game");
     }
 
     @Override
     public void clickOn() {
-
+        // TODO?
     }
 }
