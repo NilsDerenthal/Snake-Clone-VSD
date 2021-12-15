7e47462244c5171611bac6a91c236cba7be779f8
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-12-15 17:24:27 +0100

    cleanup 4

--- src/main/java/my_project/model/menu/Menu.java
+++ src/main/java/my_project/model/menu/Menu.java
@@ -15,18 +15,26 @@ public class Menu extends GraphicalObject {
 
     public Menu(ViewController viewController){
         viewController.draw(this, SceneConfig.MENU_SCENE);
-        leftList=new VisualList<>(0,50,20,40);
-        new MenuPoint(100,viewController,leftList);
+        leftList = new VisualList<>(0, 50, 20, 40);
+        new MenuPoint(100, viewController, leftList);
     }
 
+    @Override
     public void draw(DrawTool drawTool){
-        drawTool.setCurrentColor(Color.darkGray);
-        drawTool.drawFilledRectangle(0,0,Config.WINDOW_WIDTH,Config.WINDOW_HEIGHT);
-        drawTool.setCurrentColor(Color.gray);
-        drawTool.drawFilledPolygon(30,10,10,30,10,Config.WINDOW_HEIGHT-70,30,Config.WINDOW_HEIGHT-50,80,Config.WINDOW_HEIGHT-50,100,Config.WINDOW_HEIGHT-70,100,30,80,10);
-    }
+        // background
+        drawTool.setCurrentColor(Color.DARK_GRAY);
+        drawTool.drawFilledRectangle(0, 0, Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT);
 
-    public void update(double dt){
-
+        drawTool.setCurrentColor(Color.GRAY);
+        drawTool.drawFilledPolygon(
+                30, 10,
+                10, 30,
+                10, Config.WINDOW_HEIGHT - 70,
+                30, Config.WINDOW_HEIGHT - 50,
+                80, Config.WINDOW_HEIGHT - 50,
+                100, Config.WINDOW_HEIGHT - 70,
+                100, 30,
+                80, 10
+        );
     }
 }
