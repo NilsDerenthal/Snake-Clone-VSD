a7fa1bce9b3d0074a4bf177cc221b7f1bb0ba672
NilsDerenthal <derenils@kkg-do.eu>
2021-12-15 12:18:46 +0100

    enemy sceleton

--- src/main/java/my_project/model/game/Enemy.java
+++ src/main/java/my_project/model/game/Enemy.java
@@ -4,18 +4,12 @@ import KAGO_framework.view.DrawTool;
 
 public class Enemy extends Entity {
 
+    public Enemy(double alphaChangeRate) {
+        super(alphaChangeRate);
+    }
+
     @Override
     public void draw(DrawTool d) {
-
-    }
-
-    //@Override
-    public void fadeIn() {
-
-    }
-
-    //@Override
-    public void fadeOut() {
 
     }
 }
--- src/main/java/my_project/model/game/Entity.java
+++ src/main/java/my_project/model/game/Entity.java
@@ -5,10 +5,37 @@
 package my_project.model.game;
 
 import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
 import my_project.model.visual_ds.Visual2DArray;
 
 public abstract class Entity extends GraphicalObject implements Visual2DArray.Animatable {
 
+    private boolean fadingIn, fadingOut;
+    private double alpha, alphaChangeRate;
 
+    public Entity(double alphaChangeRate) {
+        this.alphaChangeRate = alphaChangeRate;
+    }
 
+    @Override
+    public abstract void draw(DrawTool drawTool);
+
+    @Override
+    public void update(double dt) {
+        double change = dt * alphaChangeRate;
+        if (fadingOut)
+            this.alpha = Math.max(0, this.alpha - change);
+        if (fadingIn)
+            this.alpha = Math.min(255, this.alpha + change);
+    }
+
+    @Override
+    public void fadeIn() {
+        fadingIn = true;
+    }
+
+    @Override
+    public void fadeOut() {
+        fadingOut = true;
+    }
 }
