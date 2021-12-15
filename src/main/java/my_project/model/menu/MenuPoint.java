a2755cc569de51a56189d28a6f2cb78802323558
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-12-15 17:11:57 +0100

    cleanup 1

--- src/main/java/my_project/model/menu/MenuPoint.java
+++ src/main/java/my_project/model/menu/MenuPoint.java
@@ -12,30 +12,45 @@ public  class MenuPoint extends GraphicalObject implements VisualList.AnimableLi
 
     private final VisualList<MenuUnderPoint> list;
     private final VisualList<MenuPoint> inList;
+
     private double sY;
 
-    public MenuPoint(double y, ViewController viewController, VisualList inList){
-        this.y=y;
-        sY=y;
+    public MenuPoint(double y, ViewController viewController, VisualList<MenuPoint> inList){
+        this.y = y;
+        this.sY = y;
+        this.inList = inList;
+
+        list = new VisualList<>(50, 0, 160, y + 30);
+        StartMenu m = new StartMenu(y);
+
         viewController.draw(this);
-        list=new VisualList<>(50,0,160,y+30);
-        this.inList=inList;
-        StartMenu m=new StartMenu(y);
         list.append(m);
         viewController.draw(m);
     }
 
-    public void draw(DrawTool drawTool){
+    @Override
+    public void draw(DrawTool drawTool) {
         drawTool.setCurrentColor(Color.gray);
-        drawTool.drawFilledPolygon(130,y+20,150,y,Config.WINDOW_WIDTH-45,y,Config.WINDOW_WIDTH-25,y+20,Config.WINDOW_WIDTH-25,y+150,Config.WINDOW_WIDTH-45,y+170,150,y+170,130,y+150);
+        // not better-looking, but easier to read
+        drawTool.drawFilledPolygon(
+                130, y+20,
+                150, y,
+                Config.WINDOW_WIDTH - 45, y,
+                Config.WINDOW_WIDTH - 25, y + 20,
+                Config.WINDOW_WIDTH - 25, y + 150,
+                Config.WINDOW_WIDTH - 45, y + 170,
+                150, y + 170,
+                130, y + 150
+        );
     }
 
+    @Override
     public void update(double dt){
-        if(y!=sY){
-            if(y<sY-1){
-                y+=10*dt;
-            }else if(y>sY+1){
-                y-=10*dt;
+        if(y != sY){
+            if(y < sY - 1){
+                y += 10 * dt;
+            } else if(y > sY + 1){
+                y -= 10 * dt;
             }
         }
     }
@@ -45,11 +60,11 @@ public  class MenuPoint extends GraphicalObject implements VisualList.AnimableLi
         return false;
     }
 
-    public void chengeY(double newY) {
-        sY=newY;
+    public void changeY(double newY) {
+        sY = newY;
         if(!list.isEmpty()){
             list.toFirst();
-            while(list.getCurrent()!=null) {
+            while(list.getCurrent() != null) {
                 list.getCurrent().changeY(newY);
                 list.next();
             }
