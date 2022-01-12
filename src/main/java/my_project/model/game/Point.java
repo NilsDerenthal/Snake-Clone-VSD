b8a528e8a4cd8031ff571e8977c6377e2f7a9030
zzxafurk <ggg@lll.de>
2022-01-12 13:11:13 +0100

    .

---
+++ src/main/java/my_project/model/game/Point.java
@@ -0,0 +1,15 @@
+package my_project.model.game;
+
+import KAGO_framework.view.DrawTool;
+
+public class Point extends Entity{
+
+    public Point(double alphaChangeRate) {
+        super(alphaChangeRate);
+    }
+
+    @Override
+    public void draw(DrawTool drawTool) {
+
+    }
+}
