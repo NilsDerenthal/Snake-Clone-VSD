b2ab2620c0c14ae5f7d460cfb62d71c55760c3ca
zzxafurk <ggg@lll.de>
2022-01-27 08:43:12 +0100

    Defeatscreen hat verschiedene reaktionen

--- src/main/java/my_project/model/game/Defeat.java
+++ src/main/java/my_project/model/game/Defeat.java
@@ -14,9 +14,32 @@ public class Defeat extends GraphicalObject {
     private ViewController viewController;
     private ProgramController programController;
 
+    private String[] flame;
+    private String[] normal;
+    private String[] praise;
+    private String chosenOne;
+
     public Defeat(ViewController viewController, ProgramController programController){
         this.viewController = viewController;
         this.programController = programController;
+
+        flame = new String[4];
+        normal = new String[4];
+        praise = new String[4];
+
+        flame[0] = "Suicide?";
+        flame[1] = "BRUH! ARE YOU REALLY THAT BAD?!?";
+        flame[2] = "YOU ARE BAD";
+        flame[3] = "(╯°□°）╯︵ ┻━┻";
+        normal[0] = "I mean like you tried...";
+        normal[1] = "YOU BE DED!";
+        normal[2] = "BEGONE!";
+        normal[3] = "You lost";
+        praise[0] = "I will approve";
+        praise[1] = "Actually...not bad";
+        praise[2] = "(☞ﾟヮﾟ)☞";
+        praise[3] = "A nice score you got there!";
+
         viewController.draw(this, DEFEAT_SCENE);
     }
 
@@ -24,6 +47,19 @@ public class Defeat extends GraphicalObject {
     public void draw(DrawTool drawTool) {
         drawTool.formatText("", 3, 40);
         drawTool.drawText(250,200,"YOU BE DED!");
+        drawTool.drawText(250,200,chosenOne);
         drawTool.drawText(260,260,"Score: " + programController.getPoint());
+    }
+
+    public void setFlame(){
+        chosenOne = flame[ (int) Math.random() * 4];
+    }
+
+    public void setNormal(){
+        chosenOne = normal[ (int) Math.random() * 4];
+    }
+
+    public void setPraise(){
+        chosenOne = praise[ (int) Math.random() * 4];
     }
 }
